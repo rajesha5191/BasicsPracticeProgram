8a5c6a0482c4b22e7e0873276887f88177d93bad
Rajesh <rajesha5191@gmail.com>
2024-09-05 16:29:35 +0530

    new folder

---
+++ LambdaExpressionPracticeProgram/practice3.java
@@ -0,0 +1,10 @@
+package Java8Features;
+
+public class practice3 {
+    public static void main(String[] args) {
+        System.out.println("class just add git repo");
+        System.out.println("Modifi the file");
+        System.out.println("add to git");
+        System.out.println("push from IDE");
+    }
+}
